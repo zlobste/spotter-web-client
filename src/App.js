f36c24c0a3bed7e73b141688ea5450920cf1e890
zlobste <nkrayniuk@gmail.com>
2021-06-10 16:35:26 +0300

    Implement auth

--- src/App.js
+++ src/App.js
@@ -1,48 +1,49 @@
-import React from 'react';
 import {
-    ChakraProvider,
-    Box,
-    Grid,
-    theme, Container,
+  ChakraProvider,
+  Box,
+  Grid,
+  theme, Container,
 } from '@chakra-ui/react';
-import {ColorModeSwitcher} from './ColorModeSwitcher';
-import {useAuth} from "./hooks/auth.hook";
-import {useRoutes} from "./hooks/route.hook";
-import {AuthContext} from './context/AuthContext'
-import {BrowserRouter as Router} from 'react-router-dom'
-import {Loader} from "./components/Loader";
-import {Footer} from "./components/Footer";
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { useAuth } from './hooks/auth.hook';
+import { useRoutes } from './hooks/route.hook';
+import { AuthContext } from './context/AuthContext';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Loader } from './components/Loader';
+import { Footer } from './components/Footer';
 
 
 function App() {
-    const {token, login, logout, userId, ready} = useAuth()
-    const isAuthenticated = true // !!token
-    const routes = useRoutes(isAuthenticated)
+  const { token, login, logout, userId, ready } = useAuth();
+  const isAuthenticated = !!token;
+  const routes = useRoutes(isAuthenticated);
 
-    if (!ready) {
-        return <Loader/>
-    }
+  if (!ready) {
+    return <Loader />;
+  }
 
-    return (
-        <ChakraProvider theme={theme}>
-            <Box textAlign="center" fontSize="xl">
-                <Grid minH="100vh" p={3}>
-                    <ColorModeSwitcher justifySelf="flex-end"/>
+  return (
+    <AuthContext.Provider value={{
+      token, login, logout, userId, isAuthenticated,
+    }}>
+      <ChakraProvider theme={theme}>
+        <Box textAlign='center' fontSize='xl'>
+          <Grid minH='100vh' p={3}>
+            <ColorModeSwitcher justifySelf='flex-end' />
 
-                    <AuthContext.Provider value={{
-                        token, login, logout, userId, isAuthenticated
-                    }}>
-                        <Router>
-                            <Container  maxW="xl" centerContent>
-                                {routes}
-                            </Container>
-                            {isAuthenticated && <Footer/> }
-                        </Router>
-                    </AuthContext.Provider>
-                </Grid>
-            </Box>
-        </ChakraProvider>
-    );
+
+            <Router>
+              <Container maxW='xl' centerContent>
+                {routes}
+              </Container>
+              {isAuthenticated && <Footer />}
+            </Router>
+
+          </Grid>
+        </Box>
+      </ChakraProvider>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,131 +1,153 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, { useContext, useState } from 'react';
 import {
-    Flex,
-    Box,
-    FormControl,
-    FormLabel,
-    Input,
-    Checkbox,
-    Stack,
-    Link,
-    Button,
-    Heading,
-    Text,
-    useColorModeValue,
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
 } from '@chakra-ui/react';
-import {useHttp} from '../hooks/http.hook'
-import {AuthContext} from '../context/AuthContext'
+import { useHttp } from '../hooks/http.hook';
+import { AuthContext } from '../context/AuthContext';
 
 export const Auth = () => {
-    const auth = useContext(AuthContext)
-    const {request, error, clearError} = useHttp()
-    const [form, setForm] = useState({
-        email: '', password: '', name: '',
-    })
-    const [formType, setFormType] = useState({login: true})
+  const auth = useContext(AuthContext);
+  const { request} = useHttp();
+  const [form, setForm] = useState({
+    email: '', password: '', name: '', surname: '',
+  });
+  const [formType, setFormType] = useState({ login: true });
 
-    useEffect(() => {
-        clearError()
-    }, [error, clearError])
+  const changeHandler = event => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
 
-    const changeHandler = event => {
-        setForm({...form, [event.target.name]: event.target.value})
-    }
+  const switchForm = () => {
+    setFormType({ login: !formType.login });
+  };
 
-    const switchForm = () => {
-        setFormType({login: !formType.login})
-    }
+  const handleRequest = async () => {
+    formType.login ? await loginHandler() : await registerHandler();
+  };
 
-    const handleRequest = async () => {
-        formType.login ? await loginHandler() : await registerHandler()
-    }
-
-    const registerHandler = async () => {
-        try {
-            await request('/api/auth/register', 'POST', {...form})
-        } catch (e) {
-            console.error(e)
+  const registerHandler = async () => {
+    try {
+      await request('auth/register', 'POST', {
+        data: {
+          name: form.name,
+          surname: form.surname,
+          email: form.email,
+          password: form.password
         }
+      });
+    } catch (e) {
+      console.error(e);
     }
+    switchForm()
+  };
 
-    const loginHandler = async () => {
-        try {
-            const data = await request('/api/auth/login', 'POST', {...form})
-            auth.login(data.token, data.userId)
-        } catch (e) {
-            console.error(e)
+  const loginHandler = async () => {
+    try {
+      const data = await request('auth/login', 'POST', {
+        data: {
+          email: form.email,
+          password: form.password
         }
+      });
+      auth.login(data.message.token, data.message.userId);
+    } catch (e) {
+      console.error(e);
     }
+  };
 
-    return (
-        <Flex
-            minH={'100vh'}
-            align={'center'}
-            justify={'center'}
-        >
-            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-                <Stack align={'center'}>
-                    <Heading fontSize={'4xl'}>
-                        {formType.login ? 'Sign in to your account' : 'Create your account'}
-                    </Heading>
-                    <Text fontSize={'lg'} color={'gray.600'}>
-                        to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
-                    </Text>
-                </Stack>
-                <Box
-                    rounded={'lg'}
-                    bg={useColorModeValue('white', 'gray.700')}
-                    boxShadow={'lg'}
-                    p={8}>
-                    <Stack spacing={4}>
-                        {
-                            formType.login ? '' :
-                                <FormControl id="name">
-                                    <FormLabel>Full name</FormLabel>
-                                    <Input
-                                        type="name"
-                                        onChange={changeHandler}
-                                    />
-                                </FormControl>
-                        }
-                        <FormControl id="email">
-                            <FormLabel>Email address</FormLabel>
-                            <Input
-                                type="email"
-                                onChange={changeHandler}
-                            />
-                        </FormControl>
-                        <FormControl id="password">
-                            <FormLabel>Password</FormLabel>
-                            <Input
-                                type="password"
-                                onChange={changeHandler}
-                            />
-                        </FormControl>
-                        <Stack spacing={10}>
-                            <Stack
-                                direction={{base: 'column', sm: 'row'}}
-                                align={'start'}
-                                justify={'space-between'}>
-                                <Checkbox>{formType.login ? 'Remember me' : 'Sign in'}</Checkbox>
-                                <Link fontSize={'md'} color={'blue.400'} onClick={switchForm}>
-                                    {formType.login ? 'Do not have account?' : 'Already have an account?'}
-                                </Link>
-                            </Stack>
-                            <Button
-                                bg={'blue.400'}
-                                color={'white'}
-                                _hover={{
-                                    bg: 'blue.500',
-                                }}
-                                onClick={handleRequest}
-                            >
-                                {formType.login ? 'Sign in' : 'Sign up'}
-                            </Button>
-                        </Stack>
-                    </Stack>
-                </Box>
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+    >
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>
+            {formType.login ? 'Sign in to your account' : 'Create your account'}
+          </Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            {
+              formType.login ? '' :
+                <>
+                  <FormControl id='name'>
+                    <FormLabel>Name</FormLabel>
+                    <Input
+                      name='name'
+                      type='name'
+                      onChange={changeHandler}
+                    />
+                  </FormControl>
+                  <FormControl id='surname'>
+                    <FormLabel>Surname</FormLabel>
+                    <Input
+                      name='surname'
+                      type='surname'
+                      onChange={changeHandler}
+                    />
+                  </FormControl>
+                </>
+            }
+            <FormControl id='email'>
+              <FormLabel>Email address</FormLabel>
+              <Input
+                name='email'
+                type='email'
+                onChange={changeHandler}
+              />
+            </FormControl>
+            <FormControl id='password'>
+              <FormLabel>Password</FormLabel>
+              <Input
+                name='password'
+                type='password'
+                onChange={changeHandler}
+              />
+            </FormControl>
+            <Stack spacing={10}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>{formType.login ? 'Remember me' : 'Sign in'}</Checkbox>
+                <Link fontSize={'md'} color={'blue.400'} onClick={switchForm}>
+                  {formType.login ? 'Do not have account?' : 'Already have an account?'}
+                </Link>
+              </Stack>
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}
+                onClick={handleRequest}
+              >
+                {formType.login ? 'Sign in' : 'Sign up'}
+              </Button>
             </Stack>
-        </Flex>
-    );
-}
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+};
